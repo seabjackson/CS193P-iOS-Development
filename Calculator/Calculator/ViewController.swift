da0effebaa96d30cbc5f355907354793bfd07911
Seab <honeyboyseab@gmail.com>
2016-08-03 23:40:59 -0400

    added buttons and a display

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -10,5 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var userIsInTheMiddleOfTyping = false
+    
+    @IBOutlet weak var display: UILabel!
+    
+    @IBAction func touchDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTyping {
+            let textCurrentlyInDisplay = display!.text!
+            display.text = textCurrentlyInDisplay + digit
+        } else {
+            display!.text = digit
+        }
+        
+        userIsInTheMiddleOfTyping = true
+    }
+    
+    @IBAction func performOperation(sender: UIButton) {
+        userIsInTheMiddleOfTyping = false
+        if let mathematicalSymbol = sender.currentTitle {
+            if mathematicalSymbol == "π" {
+                display.text = String(M_PI)
+            }
+        }
+    }
 }
 
