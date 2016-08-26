8bd8e51ceafda95c59f7bb4b26a730f55f915bb9
Seab <honeyboyseab@gmail.com>
2016-08-26 11:29:38 -0400

    added workspace and implemented a twitter fetch client

---
+++ Smashtag/Smashtag/TweetTableViewCell.swift
@@ -0,0 +1,71 @@
+//
+//  TweetTableViewCell.swift
+//  Smashtag
+//
+//  Created by lily on 8/26/16.
+//  Copyright © 2016 Seab Jackson. All rights reserved.
+//
+
+import UIKit
+import Twitter
+
+class TweetTableViewCell: UITableViewCell
+{
+    
+    @IBOutlet weak var tweetScreenNameLabel: UILabel!
+    @IBOutlet weak var tweetTextLabel: UILabel!
+    @IBOutlet weak var tweetCreatedLabel: UILabel!
+    @IBOutlet weak var tweetProfileImageView: UIImageView!
+    
+    var tweet: Twitter.Tweet? {
+        didSet {
+            updateUI()
+        }
+    }
+    
+    private func updateUI() {
+        // reset any existing tweet information
+        tweetTextLabel?.attributedText = nil
+        tweetScreenNameLabel?.text = nil
+        tweetProfileImageView?.image = nil
+        tweetCreatedLabel.text = nil
+        
+        // load new information from our tweet (if any)
+        if let tweet = self.tweet
+        {
+            tweetTextLabel?.text = tweet.text
+            if tweetTextLabel?.text != nil {
+                for _ in tweet.media {
+                    tweetTextLabel.text! += " 📷"
+                }
+            }
+            
+            tweetScreenNameLabel?.text = "\(tweet.user)"
+            
+            if let profileImageURL = tweet.user.profileImageURL {
+                if let imageData = NSData(contentsOfURL: profileImageURL) {
+                    tweetProfileImageView?.image = UIImage(data: imageData)
+                }
+            }
+            
+            let formatter = NSDateFormatter()
+            if NSDate().timeIntervalSinceDate(tweet.created) > 24*60*60 {
+                formatter.dateStyle = .ShortStyle
+            } else {
+                formatter.timeStyle = .ShortStyle
+            }
+            
+            tweetCreatedLabel?.text = formatter.stringFromDate(tweet.created)
+            
+        }
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+    
+}
